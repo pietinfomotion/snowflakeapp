73ecb3ba6404786a9db8f7362cd942b22fc5fe7d
pietinfomotion <129960056+pietinfomotion@users.noreply.github.com>
2023-04-05 14:47:30 +0200

    Update snowflake_app.py
    filtering fruits

--- snowflake_app.py
+++ snowflake_app.py
@@ -13,7 +13,7 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocados', 'Strawberries'])
-
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list)
